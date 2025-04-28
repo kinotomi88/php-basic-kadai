cf42d70779522e957283e9468284f0fd71c672da
kinotomi88 <mtseas11@gmail.com>
2025-04-29 00:26:44 +0900

    kadai_007を追加

---
+++ htdocs/kadai_007/associative-arrey.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+ 
+  <head>
+    <title>課題007</title>
+  </head>
+
+  <body>
+    <p>
+    <?php
+    $sample_data= ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+    print_r($sample_data);
+
+    ?>
+    </p>
+  </body>
+
+</html>

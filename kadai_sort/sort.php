908e6d894970f6c5de0cb18f205137956e73d175
kinotomi88 <mtseas11@gmail.com>
2025-05-05 17:49:34 +0900

    kadai_sortを追加

---
+++ kadai_sort/sort.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+<body>
+  <p>
+    <?php
+    // ソート関数の定義
+    function sort_2way($array, $order) {
+      if ($order === 'asc') {
+        echo '昇順にソートします<br>';
+        sort($array);
+      } elseif ($order === 'desc') {
+        echo '降順にソートします<br>';
+        rsort($array);
+      } else {
+        echo 'ソート順は "asc" か "desc" を指定してください<br>';
+        return;
+      }
+
+      foreach ($array as $num) {
+        echo "{$num} <br>";
+      }
+    }
+
+    // ソート対象の配列
+    $nums = [15, 4, 18, 23, 10];
+
+    // 関数を呼び出してソートを実行
+    sort_2way($nums, 'asc');
+    ?>
+  </p>
+
+  <p>
+    <?php
+    // 再び同じ配列で降順にソート
+    $nums = [15, 4, 18, 23, 10];
+    sort_2way($nums, 'desc');
+    ?>
+  </p>
+</body>
+</html>
